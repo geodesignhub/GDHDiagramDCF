a2478d02de76ef31a93017dbfe1d6d9a97d9dc9f
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2017-03-07 11:00:56 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Geodesign Hub Diagram Discounted Cash Flow Calculator
+This is small plugin to help undertake cash flow analysis on diagrams in Geodesign Hub. It uses the Geodesign Hub [API](http://www.geodesignhub.com/api/) to download the diagram and systems data and then provides a series of controls  
+
+![alt text][logo]
+
+[logo]: http://imgur.com/a/Wiwo2 "Geodesign Hub Diagram Discounted Cash Flow"
+
+
+
+
+---
+### Licence
+The MIT License (MIT)
+
+Copyright (c) 2016 Hrishikesh Ballal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require("express");
+var req = require('request');
+var async = require('async');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
+var csrf = require('csurf');
+var app = express();
+app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/views'));
+app.use('/assets', express.static('static'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
+
+app.get('/', function(request, response) {
+    var opts = {};
+    if (request.query.apitoken && request.query.projectid && request.query.diagramid) {
+        var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+        // var baseurl = 'http://local.dev:8000/api/v1/projects/';
+        var apikey = request.query.apitoken;
+        var cred = "Token " + apikey;
+        var projectid = request.query.projectid;
+        var diagramid = request.query.diagramid;
+        var diagramdetailurl = baseurl + projectid + '/diagrams/' + diagramid + '/';
+        var systemsurl = baseurl + projectid + '/systems/';
+
+        var URLS = [diagramdetailurl, systemsurl];
+
+        async.map(URLS, function(url, done) {
+            req({
+                url: url,
+                headers: {
+                    "Authorization": cred,
+                    "Content-Type": "application/json"
+                }
+            }, function(err, response, body) {
+                if (err || response.statusCode !== 200) {
+                    return done(err || new Error());
+                }
+                return done(null, JSON.parse(body));
+            });
+        }, function(err, results) {
+            if (err) return response.sendStatus(500);
+
+            var diagramdetail = results[0];
+            var systemdetailurl = baseurl + projectid + '/systems/' + diagramdetail['sysid'] + '/';
+
+            var sURls = [systemdetailurl];
+
+            async.map(sURls, function(url, done) {
+                req({
+                    url: url,
+                    headers: {
+                        "Authorization": cred,
+                        "Content-Type": "application/json"
+                    }
+                }, function(err, response, body) {
+                    if (err || response.statusCode !== 200) {
+                        return done(err || new Error());
+                    }
+                    return done(null, JSON.parse(body));
+                });
+            }, function(err, sysdetails) {
+                if (err) return response.sendStatus(500);
+
+                opts = {
+                    "csrfToken": request.csrfToken(),
+                    "apitoken": request.query.apitoken,
+                    "projectid": request.query.projectid,
+                    "status": 1,
+                    "diagramdetail": JSON.stringify(results[0]),
+                    "systems": JSON.stringify(results[1]),
+                    "systemdetail": JSON.stringify(sysdetails[0]),
+                };
+                response.render('diagdcf', opts);
+            });
+
+        });
+
+    } else {
+        opts = { 'csrfToken': request.csrfToken(), 'systemdetail': '0', 'apitoken': '0', 'projectid': '0', "diagramdetail": '0', 'systems': '0' };
+        response.render('diagdcf', opts);
+    }
+
+});
+
+
+
+app.listen(process.env.PORT || 5001);
